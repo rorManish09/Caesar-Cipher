4f808974dabc56feb2e9321459c27fc1ae2dd68b
Manish Chaudhari <122202605+rorManish09@users.noreply.github.com>
2023-03-18 14:31:53 +0530

    Add files via upload

--- 8_CesarCipher.py
+++ 8_CesarCipher.py
@@ -4,7 +4,7 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#Here is the Encrypty function
+#Here is the Encryption function
 def encrypt(text,shift):
     encrypted_Text=""
     for i in text:
@@ -18,6 +18,31 @@ def encrypt(text,shift):
 
         encrypted_Text+=new_letter
     print(f"The encoded text is {encrypted_Text}")
-#Here we call our funtion
-encrypt(text,shift)
-    
+
+
+
+#Here is the Decryption Function
+def decrypt(text,shift):
+    decrypted_Text=""
+    for i in text:
+        position=alphabet.index(i)
+        new_position=position-shift
+        if new_position<0:
+            new_position+=position
+            new_letter=alphabet[new_position]
+        else:
+            new_letter=alphabet[new_position]
+        decrypted_Text+=new_letter
+    print(f"The Decoded text is {decrypted_Text}")
+
+#Here We call the funciton
+#If your choose any function it should call the corresponding function
+
+
+if direction=="encode":
+    encrypt(text, shift)
+elif direction=="decode":       
+    decrypt(text, shift)
+
+else:     
+    print("Wrong Choice")
